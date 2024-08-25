40f49c59125f7fe4f15972b73e62275815a2c7d5
kareem hamed <khidmt55@gmail.com>
2024-08-25 23:29:17 +0300

    Adding entities

---
+++ LMSApi/Database/Enitities/StudentSubject.cs
@@ -0,0 +1,11 @@
+﻿namespace LMSApi.Database.Enitities
+{
+    public class StudentSubject
+    {
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+        public int SubjectId { get; set; }
+        public Subject Subject { get; set; }
+
+    }
+}
---
+++ LMSApi/Database/Enitities/User.cs
@@ -0,0 +1,17 @@
+﻿namespace LMSApi.Database.Enitities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public List<Role> Roles { get; set; } = new List<Role>();
+        public Support? Support { get; set; }
+        public Teacher? Teacher { get; set; }
+        public Student? Student { get; set; }
+        public List<UserRole> UserRoles { get; set; } = new List<UserRole>();
+    }
+}
