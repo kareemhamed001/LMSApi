47f09ac32e77657e42095251ba26c8ef0e11b305
kareem hamed <khidmt55@gmail.com>
2024-08-26 00:28:54 +0300

    adding loging and auth requests

---
+++ LMSApi/App/Requests/LoginRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LMSApi.App.Requests
+{
+    public class LoginRequest
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$", ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, and one digit")]
+        public string Password { get; set; }
+    }
+}
