0ef730279ddf325edf5fa61131349750c9b97fe0
kareem hamed <khidmt55@gmail.com>
2024-08-26 00:28:37 +0300

    Adding Auth Controller

---
+++ LMSApi/App/Controllers/Auth/IAuthController.cs
@@ -0,0 +1,12 @@
+﻿using LMSApi.App.Requests;
+using LMSApi.App.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMSApi.App.Controllers.Auth
+{
+    public interface IAuthController
+    {
+        public Task<ActionResult<ApiResponse<string>>> Login(LoginRequest request);
+        public Task<ActionResult<ApiResponse<string>>> Register([FromBody] UserRequest request);
+    }
+}
