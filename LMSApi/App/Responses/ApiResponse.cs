6cbb0a5b66f0c289f8850733dda06e06b7fb992d
kareem hamed <khidmt55@gmail.com>
2024-08-26 00:29:05 +0300

    Adding api response

---
+++ LMSApi/App/Responses/ApiResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace LMSApi.App.Responses
+{
+    public class ApiResponse<T>
+    {
+        public List<T> Data { get; set; }
+        public string Message { get; set; }
+        public bool Success { get; set; }
+        public int Status { get; set; }
+
+    }
+}
