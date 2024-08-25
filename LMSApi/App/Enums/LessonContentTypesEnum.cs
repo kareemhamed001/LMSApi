b560b44c7822e2173a2a6ebb92b850659417aa5e
kareem hamed <khidmt55@gmail.com>
2024-08-25 23:30:04 +0300

    adding lesson content type enum

---
+++ LMSApi/App/Enums/LessonContentTypesEnum.cs
@@ -0,0 +1,15 @@
+﻿namespace LMSApi.App.Enums
+{
+    public enum LessonContentTypesEnum
+    {
+        PDF = 1,
+        Video = 2,
+        Audio = 3,
+        Image = 4,
+        Text = 5,
+        Quiz = 6,
+        Assignment = 7,
+        Code = 8,
+        Link = 9,
+    }
+}
