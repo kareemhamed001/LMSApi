26527b8828687e7ccc3f74e85d13c48415a8d923
kareem hamed <khidmt55@gmail.com>
2024-08-27 05:12:33 +0300

    adding null relations

--- LMSApi/Database/Enitities/Teacher.cs
+++ LMSApi/Database/Enitities/Teacher.cs
@@ -8,8 +8,8 @@
         public string Email { get; set; } = null!;
         public int UserId { get; set; }
         public User User { get; set; } = null!;
-        public List<Course> Courses { get; set; } = new List<Course>();
-        public List<Subscription> Subscriptions { get; set; } = new List<Subscription>();
+        public List<Course> Courses { get; set; }
+        public List<Subscription> Subscriptions { get; set; } 
         public List<Subject> Subjects { get; set; } = new List<Subject>();
         public List<TeacherSubject> TeacherSubjects { get; set; } = new List<TeacherSubject>();
 
