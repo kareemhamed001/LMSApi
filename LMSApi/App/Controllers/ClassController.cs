9a113897a8661257c359d54342e055404cbc4420
kareem hamed <khidmt55@gmail.com>
2024-08-30 20:33:34 +0300

    changing class namespace

--- LMSApi/App/Controllers/ClassController.cs
+++ LMSApi/App/Controllers/ClassController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using LMSApi.App.DTOs;
-using LMSApi.App.Interfaces.Class;
+using LMSApi.App.Interfaces;
 using LMSApi.App.Requests.Class;
 using AutoMapper;
 using LMSApi.App.Attributes;
