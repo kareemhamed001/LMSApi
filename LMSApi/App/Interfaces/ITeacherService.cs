2afdd7be7c05126bab06d56e38cf60923aaa04af
kareem hamed <khidmt55@gmail.com>
2024-08-30 20:36:10 +0300

    adding teacher controller and teacher service interfaces

---
+++ LMSApi/App/Interfaces/ITeacherService.cs
@@ -0,0 +1,18 @@
+﻿using LMSApi.App.Enums;
+using LMSApi.App.Requests.Teacher;
+using LMSApi.App.Responses.Teacher;
+using TeacherEntity = LMSApi.Database.Enitities.Teacher;
+
+namespace LMSApi.App.Interfaces
+{
+    public interface ITeacherService
+    {
+
+        public Task<(ServicesMethodsResponsesEnum, List<TeacherIndexResponse>?)> Index();
+        public Task<(ServicesMethodsResponsesEnum, TeacherEntity?, string message)> Store(CreateTeacherRequest teacherRequest);
+        public Task<(ServicesMethodsResponsesEnum, TeacherEntity?)> Update(int teacherId, UpdateTeacherRequest teacherRequest);
+        public Task<(ServicesMethodsResponsesEnum, TeacherEntity?)> Show(int teacherId);
+        public Task<ServicesMethodsResponsesEnum> Delete(int teacherId);
+
+    }
+}
