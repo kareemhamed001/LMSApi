48823912018e5eb76709694800f844b0f3de8dc0
kareem hamed <khidmt55@gmail.com>
2024-08-30 20:34:23 +0300

    adding not found exception

---
+++ LMSApi/App/Exceptions/NotFoundException.cs
@@ -0,0 +1,17 @@
+﻿namespace LMSApi.App.Exceptions
+{
+    public class NotFoundException : System.Exception
+    {
+        public NotFoundException(string message) : base(message)
+        {
+        }
+
+        public NotFoundException(string message, System.Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public NotFoundException()
+        {
+        }
+    }
+}
